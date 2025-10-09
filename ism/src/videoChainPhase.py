e09a0cc4677a9dde3a92fa1224fc7026e5395649
Víctor <100451534@alumnos.uc3m.es>
2025-10-09 18:47:36 +0200

    Full Video Chain Phase

--- ism/src/videoChainPhase.py
+++ ism/src/videoChainPhase.py
@@ -56,6 +56,7 @@ class videoChainPhase(initIsm):
         :return: output toa in [V]
         """
         #TODO
+        toa = toa * OCF * gain_adc
         return toa
 
     def digitisation(self, toa, bit_depth, min_voltage, max_voltage):
@@ -68,5 +69,7 @@ class videoChainPhase(initIsm):
         :return: toa in digital counts
         """
         #TODO
+        max_digital = (2**bit_depth) - 1
+        toa_dn = np.round((toa / (max_voltage - min_voltage)) * max_digital)
+        toa_dn = np.clip(toa_dn, 0, max_digital)
         return toa_dn
-
